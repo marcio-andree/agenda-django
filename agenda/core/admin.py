d28cf40c28d590a7fd71c796af9818c3dc29bbe3
Marcio André <andremarcio1988@gmail.com>
2022-08-29 14:53:12 -0300

    inserido campo usuário na tabela, e criado um list_filter

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -4,6 +4,6 @@ from core.models import Evento #tem que importar os models e registra los
 # Register your models here.
 class EventoAdmin(admin.ModelAdmin):
   list_display = ('titulo','descricao','data_evento','data_criacao')
-
+  list_filter = ('usuario',) # Adiciona filtro por titulo da classe evento
 
 admin.site.register(Evento, EventoAdmin)
