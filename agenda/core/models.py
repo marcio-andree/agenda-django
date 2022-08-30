245852447038771d154dbff965fd7cc9ad0c287f
Marcio André <andremarcio1988@gmail.com>
2022-08-30 11:52:24 -0300

    Criado tela de listagem de eventos

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -12,3 +12,7 @@ class Evento(models.Model):
 
     def __str__(self):
       return self.titulo
+
+
+    def get_data_evento(self):
+      return self.data_evento.strftime('%d/%m/%Y %H: %M Horas')
--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from core.models import Evento
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda/') função para redirecionar o localhost para agenda
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all() # .filter(usuario=usuario) filtra os agendamentos por usuário
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
+
