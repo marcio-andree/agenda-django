753f03ef81268e1ee147fac6b8eaed3ca99b3a40
Marcio André <andremarcio1988@gmail.com>
2022-09-05 16:33:27 -0300

    acrescentado update e delete

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -3,7 +3,7 @@ from core.models import Evento #tem que importar os models e registra los
 
 # Register your models here.
 class EventoAdmin(admin.ModelAdmin):
-  list_display = ('titulo','descricao','data_evento','data_criacao')
+  list_display = ('id','titulo','descricao','data_evento','data_criacao')
   list_filter = ('usuario',) # Adiciona filtro por titulo da classe evento
 
 admin.site.register(Evento, EventoAdmin)
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -16,3 +16,7 @@ class Evento(models.Model):
 
     def get_data_evento(self):
       return self.data_evento.strftime('%d/%m/%Y %H: %M Horas')
+
+
+    def get_data_input_evento(self):
+      return self.data_evento.strftime('%Y-%m-%dT%H:%M')
